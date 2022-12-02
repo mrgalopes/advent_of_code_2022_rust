bb5678bf4d3dc83a68ae3b8c8f33d9b9bcd85ba6
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2022-12-01 22:08:26 -0300

    Remove elf struct from day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,42 +3,30 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-#[derive(Clone, Debug)]
-struct Elf {
-    calories: Vec<u32>,
-}
-
-impl Elf {
-    fn new() -> Elf {
-        Elf {
-            calories: Vec::new(),
-        }
-    }
-}
-
 fn main() {
     let file = File::open("inputs/input1.txt").expect("File not found");
     let reader = BufReader::new(file);
 
-    let mut elves: Vec<Elf> = vec![];
-
-    let mut elf = Elf::new();
+    let mut elves: Vec<Vec<u32>> = vec![];
+    let mut elf_calories: Vec<u32> = vec![];
 
     for line in reader.lines() {
-        let line = line.expect("Error in line");
-        let trimmed_line = line.trim();
-        if trimmed_line == "" {
-            elves.push(elf.clone());
-            elf = Elf::new();
-        } else {
-            let calory: u32 = line.parse().expect("Error parsing number");
-            elf.calories.push(calory);
+        if let Ok(line) = line {
+            let trimmed_line = line.trim();
+
+            if trimmed_line == "" {
+                elves.push(elf_calories);
+                elf_calories = vec![];
+            } else {
+                let calory: u32 = line.parse().expect("Error parsing number");
+                elf_calories.push(calory);
+            }
         }
     }
 
     let mut calories: Vec<u32> = elves
         .iter()
-        .map(|elf| elf.calories.iter().sum::<u32>())
+        .map(|elf_cals| elf_cals.iter().sum::<u32>())
         .collect();
     calories.sort();
 
