2397f9d61b896d36c90298eeb449442d887830cc
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2022-12-04 12:54:03 -0300

    Solves day 4 part 1

---
+++ day04/src/main.rs
@@ -0,0 +1,93 @@
+use std::{error::Error, fs::File, io::{BufReader, BufRead}, num::ParseIntError};
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Assignment {
+    start: u32,
+    end: u32
+}
+
+fn part_1(input_file: &str) -> Result<(), Box<dyn Error>> {
+    let file = File::open(input_file)?;
+    let reader = BufReader::new(file);
+
+    let pairs_fully_containing = reader.lines()
+        .filter_map(|line| {
+            if let Ok(line) = line {
+                if let Ok(assignments) = parse_line(&line) {
+                    Some(assignments)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .filter(|(a1, a2)| fully_contains(&a1, &a2))
+        .count();
+
+    println!("{:?}", pairs_fully_containing);
+    
+    Ok(())
+}
+
+fn parse_line(line: &str) -> Result<(Assignment, Assignment), Box<dyn Error>> {
+    let assignment_strs: Vec<&str> = line.split(",").take(2).collect();
+    if assignment_strs.len() < 2 {
+        return Err(Box::from("Invalid line: {line}"));
+    }
+
+    let mut assignments: [Assignment; 2] = [Assignment{start: 0, end: 0}; 2]; 
+
+    for (i, assignment_str) in assignment_strs.iter().enumerate() {
+        let assignment_nums: Result<Vec<u32>, ParseIntError> = assignment_str
+            .split("-")
+            .take(2)
+            .map(|s| s.parse())
+            .collect();
+        let assignment_nums = assignment_nums?;
+        let assignment = Assignment {
+            start: assignment_nums[0],
+            end: assignment_nums[1]
+        };
+        assignments[i] = assignment;
+    }
+
+    Ok((assignments[0], assignments[1]))
+}
+
+fn fully_contains(first: &Assignment, second: &Assignment) -> bool {
+    (first.start >= second.start && first.end <= second.end)
+    || (second.start >= first.start && second.end <= first.end)
+}
+
+fn main() {
+    if let Err(err) = part_1("input/input.txt") {
+        println!("{:?}", err);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{Assignment, fully_contains, parse_line};
+
+    #[test]
+    fn should_parse_line() {
+        let line = "2-4,6-8";
+        let expected = (Assignment { start: 2, end: 4 }, Assignment { start: 6, end: 8 });
+
+        let result = parse_line(line);
+        assert!(result.is_ok());
+        let result = result.unwrap();
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn should_check_if_assignment_is_within_another() {
+        let within = (Assignment { start: 2, end: 8 }, Assignment { start: 3, end: 7});
+
+        assert!(fully_contains(&within.0, &within.1));
+        assert!(fully_contains(&within.1, &within.0));
+    }
+}
